26f5825f35f6dcf4a10f06a0631370f33d2f2579
Gabriel Naiman <gnaiman@keepcon.com>
2013-06-03 11:00:53 -0300

    Encode base64 cache key for method argumentes. Change to set agrument options for methods

--- lib/caching.rb
+++ lib/caching.rb
@@ -1,4 +1,6 @@
 Dir["#{File.dirname(__FILE__)}/caching/*.rb"].each { |file| require file }
+require 'set'
+require 'base64'
 
 module Caching
 
@@ -8,7 +10,8 @@ module Caching
       alias_method "#{method}_without_cache", method
 
       define_method method do |*args, &block|
-        method_key = "#{method}_#{Marshal.dump(args)}"
+        args_key = args.any? ? "_#{Base64.encode64(Marshal.dump(args))}" : ''
+        method_key = "#{method}#{args_key}"
         cached_methods_keys[method] << method_key
         cache_storage.fetch(method_key) { send "#{method}_without_cache", *args, &block }
       end
@@ -23,15 +26,15 @@ module Caching
     end
 
     def cached_methods_keys
-      @cached_methods_keys ||= Hash.new {|h,k| h[k] = []}
+      @cached_methods_keys ||= Hash.new {|h,k| h[k] = Set.new}
     end
 
     def clear_cache(*methods)
       method_keys = cached_methods_keys.
         select{ |m,_| methods.include? m }.
-        flat_map{ |_,keys| keys }
-
+        flat_map{ |_,keys| keys.to_a }
       cache_storage.clear *method_keys
+      method_keys.each { |k| cached_methods_keys.delete k }
     end
 
   end
